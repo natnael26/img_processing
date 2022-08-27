df28d57202efa062d1cdbed0ce7917c17c32239c
natnael26 <natnaeldessalegn26@gmail.com>
2022-08-27 12:16:17 +0300

    third commit

--- src/server.ts
+++ src/server.ts
@@ -35,11 +35,11 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Root Endpoint
   // Displays a simple message to the user
   
-  app.get( "/", async ( req, res ) => {
+  app.get( "/", async ( req:express.Request, res:express.Response ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
-  
-  app.get('/filteredimage', async (req,res) => {
+
+  app.get('/filteredimage', async (req:express.Request, res:express.Response) => {
     const image_url = req.query.image_url.toString();
     if (!image_url) {
       res.status(400).send('Image url is required');
